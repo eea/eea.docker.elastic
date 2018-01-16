e29a0c790e8ed20239a483a4dbb59f46b3e1eba5
valentinab25 <valentinab25>
2018-01-16 13:06:35 +0100

    removed plugin from non-http nodes

--- Dockerfile
+++ Dockerfile
@@ -4,8 +4,7 @@ COPY plugins/readonlyrest-1.16.15_es6.1.1.zip /tmp/
 
 
 RUN /usr/share/elasticsearch/bin/elasticsearch-plugin remove x-pack \
-    && /usr/share/elasticsearch/bin/elasticsearch-plugin install analysis-icu \
-    && /usr/share/elasticsearch/bin/elasticsearch-plugin install file:/tmp/readonlyrest-1.16.15_es6.1.1.zip
+    && /usr/share/elasticsearch/bin/elasticsearch-plugin install analysis-icu 
     
 
 
@@ -14,4 +13,6 @@ RUN mv /usr/local/bin/docker-entrypoint.sh /usr/local/bin/elastic-entrypoint.sh
 COPY docker-entrypoint.sh /usr/local/bin/docker-entrypoint.sh
 
 COPY config /usr/share/elasticsearch/config
+
+RUN mv /usr/share/elasticsearch/config/readonlyrest.yml /tmp
 
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,26 +3,37 @@ set -e
 
 
 
-if [ $(env | grep -c "http.enabled=true") -ne 0 ]; then
+if [ $(env | grep -c "http.enabled=false") -eq 0 ]; then
     if [ ! -f /usr/share/elasticsearch/ssl/self.jks ]; then
       mkdir -p /usr/share/elasticsearch/ssl
       keytool -genkey -keyalg RSA -noprompt -alias $HOSTNAME -dname "CN=$HOSTNAME,OU=IDM,O=EEA,L=IDM1,C=DK" -keystore /usr/share/elasticsearch/ssl/self.jks -storepass $KEYSTORE_PASSWORD -keypass $KEY_PASSWORD
       keytool -keystore  /usr/share/elasticsearch/ssl/self.jks -alias $HOSTNAME -export -file  /usr/share/elasticsearch/ssl/self.cert
     fi
+
+    
+    if [ -f /tmp/readonlyrest-1.16.15_es6.1.1.zip ]; then
+    /usr/share/elasticsearch/bin/elasticsearch-plugin install file:/tmp/readonlyrest-1.16.15_es6.1.1.zip
+
+    mv /tmp/readonlyrest.yml /usr/share/elasticsearch/config/readonlyrest.yml
+
+    sed -i "s/KEYSTORE_PASSWORD/$KEYSTORE_PASSWORD/g" /usr/share/elasticsearch/config/readonlyrest.yml
+
+    sed -i "s/KEY_PASSWORD/$KEY_PASSWORD/g" /usr/share/elasticsearch/config/readonlyrest.yml
+
+    sed -i "s/RW_USER/$RW_USER/g" /usr/share/elasticsearch/config/readonlyrest.yml
+    sed -i "s/RW_PASSWORD/$RW_PASSWORD/g" /usr/share/elasticsearch/config/readonlyrest.yml
+
+    sed -i "s/RO_USER/$RO_USER/g" /usr/share/elasticsearch/config/readonlyrest.yml
+    sed -i "s/RO_PASSWORD/$RO_PASSWORD/g" /usr/share/elasticsearch/config/readonlyrest.yml
+
+    sed -i "s/KIBANA_USER/$KIBANA_USER/g" /usr/share/elasticsearch/config/readonlyrest.yml
+    sed -i "s/KIBANA_PASSWORD/$KIBANA_PASSWORD/g" /usr/share/elasticsearch/config/readonlyrest.yml
+
+    rm -f /tmp/readonlyrest-1.16.15_es6.1.1.zip
+
+    fi
 fi
 
-sed -i "s/KEYSTORE_PASSWORD/$KEYSTORE_PASSWORD/g" /usr/share/elasticsearch/config/readonlyrest.yml
-
-sed -i "s/KEY_PASSWORD/$KEY_PASSWORD/g" /usr/share/elasticsearch/config/readonlyrest.yml
-
-sed -i "s/RW_USER/$RW_USER/g" /usr/share/elasticsearch/config/readonlyrest.yml
-sed -i "s/RW_PASSWORD/$RW_PASSWORD/g" /usr/share/elasticsearch/config/readonlyrest.yml
-
-sed -i "s/RO_USER/$RO_USER/g" /usr/share/elasticsearch/config/readonlyrest.yml
-sed -i "s/RO_PASSWORD/$RO_PASSWORD/g" /usr/share/elasticsearch/config/readonlyrest.yml
-    
-sed -i "s/KIBANA_USER/$KIBANA_USER/g" /usr/share/elasticsearch/config/readonlyrest.yml
-sed -i "s/KIBANA_PASSWORD/$KIBANA_PASSWORD/g" /usr/share/elasticsearch/config/readonlyrest.yml
 
 
 exec /usr/local/bin/elastic-entrypoint.sh $@
