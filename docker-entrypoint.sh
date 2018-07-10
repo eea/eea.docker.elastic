08521df0b32462eb0c943f273c262f4ccf30ebf1
valentinab25 <valentinab25>
2018-07-10 16:14:31 +0300

    Added certificate generation only for ssl enabled

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,10 +4,11 @@ set -e
 
 
 if [ $(env | grep -c "http.enabled=false") -eq 0 ]; then
-    if [ ! -f /usr/share/elasticsearch/ssl/self.jks ]; then
+
+    if [ $(env | grep -c "http.type=ssl_netty4") -eq 1 ] && [ ! -f /usr/share/elasticsearch/ssl/self.jks ]; then
       mkdir -p /usr/share/elasticsearch/ssl
-      keytool -genkey -keyalg RSA -noprompt -alias $HOSTNAME -dname "CN=$HOSTNAME,OU=IDM,O=EEA,L=IDM1,C=DK" -keystore /usr/share/elasticsearch/ssl/self.jks -storepass $KEYSTORE_PASSWORD -keypass $KEY_PASSWORD
-      keytool -keystore  /usr/share/elasticsearch/ssl/self.jks -alias $HOSTNAME -export -file  /usr/share/elasticsearch/ssl/self.cert
+      $JAVA_HOME/bin/keytool -genkey -keyalg RSA -noprompt -alias $HOSTNAME -dname "CN=$HOSTNAME,OU=IDM,O=EEA,L=IDM1,C=DK" -keystore /usr/share/elasticsearch/ssl/self.jks -storepass $KEYSTORE_PASSWORD -keypass $KEY_PASSWORD
+      $JAVA_HOME/bin/keytool -keystore  /usr/share/elasticsearch/ssl/self.jks -alias $HOSTNAME -export -file  /usr/share/elasticsearch/ssl/self.cert
     fi
 
     if [ $ENABLE_READONLY_REST == "true" ]; then
