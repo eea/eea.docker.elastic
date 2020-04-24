ad4a50a5b34381cff9326f3fa5a5f1e92b3907a5
valentinab25 <valentinab25>
2020-04-24 14:02:50 +0300

    Revert last 3 commits

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,8 @@
 FROM docker.elastic.co/elasticsearch/elasticsearch:6.8.8
 
+COPY plugins/readonlyrest-1.19.4_es6.8.8.zip /tmp/
+
+
 RUN /usr/share/elasticsearch/bin/elasticsearch-plugin install analysis-icu 
     
 RUN mv /usr/local/bin/docker-entrypoint.sh /usr/local/bin/elastic-entrypoint.sh
@@ -7,3 +10,6 @@ RUN mv /usr/local/bin/docker-entrypoint.sh /usr/local/bin/elastic-entrypoint.sh
 COPY docker-entrypoint.sh /usr/local/bin/docker-entrypoint.sh
 
 COPY config /usr/share/elasticsearch/config
+
+RUN mv /usr/share/elasticsearch/config/readonlyrest.yml /tmp
+
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -11,6 +11,44 @@ if [ $(env | grep -c "http.enabled=false") -eq 0 ]; then
       $JAVA_HOME/bin/keytool -keystore  /usr/share/elasticsearch/ssl/self.jks -alias $HOSTNAME -export -file  /usr/share/elasticsearch/ssl/self.cert
     fi
 
+    if [ "$ENABLE_READONLY_REST" == "true" ]; then
+        if [ -f /tmp/readonlyrest-* ]; then
+            plugin_name=$(ls /tmp/readonlyrest-*.zip)
+            /usr/share/elasticsearch/bin/elasticsearch-plugin install --batch file://$plugin_name 
+
+            mv /tmp/readonlyrest.yml /usr/share/elasticsearch/config/readonlyrest.yml
+
+            if [ $(env | grep -c "http.type=ssl_netty4") -eq 1 ]; then
+	       sed -i "s/KEYSTORE_PASSWORD/$KEYSTORE_PASSWORD/g" /usr/share/elasticsearch/config/readonlyrest.yml
+               sed -i "s/KEY_PASSWORD/$KEY_PASSWORD/g" /usr/share/elasticsearch/config/readonlyrest.yml
+            fi
+
+            sed -i "s/RW_USER/$RW_USER/g" /usr/share/elasticsearch/config/readonlyrest.yml
+            sed -i "s/RW_PASSWORD/$RW_PASSWORD/g" /usr/share/elasticsearch/config/readonlyrest.yml
+
+            sed -i "s/RO_USER/$RO_USER/g" /usr/share/elasticsearch/config/readonlyrest.yml
+            sed -i "s/RO_PASSWORD/$RO_PASSWORD/g" /usr/share/elasticsearch/config/readonlyrest.yml
+
+
+            if [ -n "$KIBANA_HOSTNAME" ]; then
+	        echo "
+    - name: \"::KIBANA_HOST::\"
+      kibana_access: ro_strict
+      hosts: [\"$KIBANA_HOSTNAME\"]
+      verbosity: error # don't log successful request" >> /usr/share/elasticsearch/config/readonlyrest.yml
+                if [ -n "$KIBANA_USER" ]; then
+                       echo "
+    # We trust Kibana's server side process, full access granted via HTTP authentication
+    - name: \"::KIBANA-SRV::\"
+      # auth_key is good for testing, but replace it with auth_key_sha256!
+      auth_key: $KIBANA_USER:$KIBANA_PASSWORD
+      verbosity: error # don't log successful request" >> /usr/share/elasticsearch/config/readonlyrest.yml
+                fi
+            fi
+
+            rm -f /tmp/readonlyrest-*
+        fi
+    fi
 fi
 
 #make sure that elasticsearch volume has correct permissions
